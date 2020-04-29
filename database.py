067c5790195a2d3a5f2c0804df1657a2dc98aaae
kavya76 <42600631+kavya76@users.noreply.github.com>
2020-04-29 18:52:13 +0530

    needs some ui improvement in main file

---
+++ database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+#Open database
+conn = sqlite3.connect('database.db')        
+
+#Create table
+conn.execute('''CREATE TABLE users 
+		(userId INTEGER PRIMARY KEY, 
+		password TEXT,
+		email TEXT,
+		firstName TEXT,
+		lastName TEXT,
+		address1 TEXT,
+		address2 TEXT,
+		zipcode TEXT,
+		city TEXT,
+		state TEXT,
+		country TEXT, 
+		phone TEXT
+		)''')
+
+
+
+
+conn.close()
+
